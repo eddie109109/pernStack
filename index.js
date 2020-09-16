24128c65c69fbcb377ee4ff0774432526142fdfd
jundic <jundi_chen@sfu.ca>
2020-09-16 07:01:12 -0700

    fix port

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
